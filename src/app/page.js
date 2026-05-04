95151abb066a964bbc5c15b1ffe780a9e199b992
123dd654 <rhthrck12@gmail.com>
2026-05-04 14:50:15 +0900

    이미지 로딩 최적화 재수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,47 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import logoSvg from "../../public/img/logo.svg"
+import { useEffect } from "react";
 import Image from "next/image";
+import logoSvg from "../../public/img/logo.svg";
+import { menuItems, noodles, Rice, prepare_dish, sideMenus, drinkMenus } from "@/constants/datas";
 
 export default function Home() {
   const router = useRouter();
-  setTimeout(() => {
-    router.push("/main");
-  }, 3000);
+
+  useEffect(() => {
+    // 1. 이미지 미리 받아두기 (핵심)
+    const allImages = [
+      ...menuItems,
+      ...noodles,
+      ...Rice,
+      ...prepare_dish,
+      ...sideMenus,
+      ...drinkMenus,
+    ].map(item => item.imageUrl);
+
+   allImages.forEach((src) => {
+      const img = new window.Image();
+      img.src = src;
+    });
+
+    // 2. 3초 후 이동
+    const timer = setTimeout(() => {
+      router.push("/main");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="randing_page_wrapper">
-      <Image src={logoSvg} width={30} alt="qbit logo" height={30}/>
+      <Image src={logoSvg} width={30} height={30} alt="qbit logo" />
       <div className="firtst_loading">
-        <span className="loading_span"></span>
-        <span className="loading_span"></span>
-        <span className="loading_span"></span>
-        <span className="loading_span"></span>
-        <span className="loading_span"></span>
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
       </div>
     </div>
   );
